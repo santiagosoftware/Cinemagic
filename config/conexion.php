a24a3c8c92ba353bef30ba009f50ccb8585b0fe9
Harold <haroldtecnico105@gmail.com>
2026-05-27 16:55:26 -0500

    listo

--- config/conexion.php
+++ config/conexion.php
@@ -5,8 +5,16 @@ $user = "root";
 $password = "";
 $database = "Cinemagic";
 
-$conn = mysqli_connect($host,$user,$password,$database);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password);
 
-if(!$conn){
-    die("Error de conexión");
+    // Mostrar errores de PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Conexión exitosa";
+
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
 }
+
+?>
